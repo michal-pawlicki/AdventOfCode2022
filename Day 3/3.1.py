ddaa07945bdff97660247e10aa0acc1ac04c20f2
Michał Pawlicki <106492629+michal-pawlicki@users.noreply.github.com>
2022-12-04 11:25:16 +0100

    Add files via upload

---
+++ Day 3/3.1.py
@@ -0,0 +1,11 @@
+file = open("data", "r")
+lines = file.readlines()
+result = 0
+for line in lines:
+    for element in "".join(set(line[0:len([*line])//2])):
+        if element in [*line[len([*line])//2:]]:
+            if element.isupper():
+                result += ord(element) - 38
+            else:
+                result += ord(element) - 96
+print(result)
---
+++ Day 4/4.1.py
@@ -0,0 +1,10 @@
+file = open("data", "r")
+lines = file.readlines()
+result = 0
+for line in lines:
+    arr = [i.split('-') for i in line.strip().split(",")]
+    A = set(range(int(arr[0][0]), int(arr[0][1])+1))
+    B = set(range(int(arr[1][0]), int(arr[1][1])+1))
+    if A.issubset(B) or B.issubset(A):
+        result += 1
+print(result)
